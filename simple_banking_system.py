dae91d112cd9e8657e0ad6b8fe6d9f9a2388e665
patrykgiertych <patrykgiertych1@gmail.com>
2021-05-10 14:33:17 +0200

    luhn, OOP

--- simple_banking_system.py
+++ simple_banking_system.py
@@ -1,59 +1,97 @@
 import random
 
 class CreditCard:
-    def __init__(self, balance = 0):
-        self.card_id = '400000'
-        while len(self.card_id) < 16:
-            self.card_id += str(random.randint(0,9))
-        self.card_pin = ''
-        while len(self.card_pin) < 4:
-            self.card_pin += str(random.randint(0,9))
-        self.balance = 0
+    card_id = None
+    card_pin = None
+    balance = 0
 
-    def card_id_pin(self, card):
-        print(card.card_id)
-        print(card.card_pin)
+    def create_id():
+        CreditCard.card_id = '400000'
+        while len(CreditCard.card_id) < 15:
+            CreditCard.card_id += str(random.randint(0,9))
+        count = 1
+        multiplied = []
+        minus= []
+        for i in CreditCard.card_id:
+            if count % 2 != 0:
+                i = int(i) * 2
+                multiplied.append(i)
+            else:
+                multiplied.append(i)
+            count += 1
+        for i in multiplied:
+            if int(i) > 9:
+                i = int(i) - 9
+                minus.append(i)
+            else:
+                minus.append(i)
+        digits_sum = sum([int(i) for i in minus])
+        if digits_sum % 10 == 0:
+            CreditCard.card_id += '0'
+        else:
+            rem = digits_sum % 10
+            checksum = 10 - rem
+            CreditCard.card_id += str(checksum)
 
-def loop_exit():
-    print('Bye')
-    exit()
-
-
-while True:
-    print('1. Create an account\n2. Log into account\n0. Exit')
-    user = input()
-    print()
-    if user == '1':
-        card_1 = CreditCard()
+    def create_pin():
+        CreditCard.card_pin = ''
+        while len(CreditCard.card_pin) < 4:
+            CreditCard.card_pin += str(random.randint(0,9))
+    
+    def create_credit_card():
+        CreditCard.create_id()
+        CreditCard.create_pin()
         print('Your card has been created')
-        print(f'Your card number:\n{card_1.card_id}')
-        print(f'Your card pin:\n{card_1.card_pin}')
-        continue
-    elif user == '2':
-        logged_in = False
-        while True:
-            while not logged_in:
-                print('Enter your card number:')
-                card_number = input()
-                print('Enter your PIN:')
-                card_pin = input()
-                if card_number == card_1.card_id and card_pin == card_1.card_pin:
-                    print('You have successfully logged in!')
-                    logged_in = True
-                    break
-                else:
-                    print('Wrong card number or PIN!')
-                    break
-            print('\n1. Balance\n2. Log out\n0. Exit')
-            user = input()
-            print()
-            if user == '1':
-                print(f'Balance: {card_1.balance}')
-                continue
-            elif user == '2':
-                print('You have successfully logged out!')
-                break
-            elif user == '0':
-                loop_exit()
-    elif user == '0':
-        loop_exit()
+        print(f'Your card number:\n{CreditCard.card_id}')
+        print(f'Your card pin:\n{CreditCard.card_pin}')
+        CreditCard.main_menu()
+
+    def main_menu():
+        print('1. Create an account\n2. Log into account\n0. Exit\n')
+        user = input()   
+        if user == '1':
+            CreditCard.create_credit_card()
+        elif user == '2':
+            CreditCard.login()
+        elif user == '0':
+            CreditCard.exit()
+    
+    def login():
+        print('Enter your card number:')
+        card_number = input()
+        print('Enter your PIN:')
+        card_pin = input()
+        if card_number == CreditCard.card_id and card_pin == CreditCard.card_pin:
+            print('You have successfully logged in!\n')
+            CreditCard.logged_in()
+        else:
+            print('Wrong card number or PIN!')
+            CreditCard.main_menu()
+
+    def logged_in():
+        print('\n1. Balance\n2. Log out\n0. Exit\n')
+        user = input()
+        if user == '1':
+            print(f'Balance: {CreditCard.balance}')
+            CreditCard.logged_in()
+        elif user == '2':
+            print('You have successfully logged out!')
+            CreditCard.main_menu()
+        elif user == '0':
+            CreditCard.exit()
+
+    def exit():
+        print('Bye')
+        quit()
+
+
+
+
+CreditCard.main_menu()
+
+
+
+
+
+
+
--- test.py
+++
@@ -1,8 +0,0 @@
-import random
-
-# class CreditCard:
-#     def __init__(self):
-#         self.card_id = '400000' + random.randint(1000000000000)
-
-string = '40' + str(random.randint(0, 10000))
-print(string)
